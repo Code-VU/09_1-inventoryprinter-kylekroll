85b70e9538f0c1a17eab86c2a9e50dd245200275
Kyle Kroll <Kyle.Kroll@vu.com>
2023-04-02 15:23:17 -0500

    Completed assignment to specifications

--- displayInventory.py
+++ displayInventory.py
@@ -2,6 +2,17 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
+    
+    x = None
+    print("Inventory:")
+    for item,value in inventory.items():
+        if x is None:
+            x = value
+        else:
+            x = x + value
+        print(value, item)
+    print("Total number of items:", x)
+
 
 
 if __name__ == "__main__":
